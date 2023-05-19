79f895c0f97b56eeb34ad1d9d85cf3e6157bb860
CptTemplar1 <michal2001k@op.pl>
2023-05-19 12:34:03 +0200

    Dodano działającą krew po uderzeniu mieczem

--- PGK_lab_URP/Assets/Scripts/PlayerAttack.cs
+++ PGK_lab_URP/Assets/Scripts/PlayerAttack.cs
@@ -112,7 +112,34 @@ public class PlayerAttack : MonoBehaviour
         audioSource.PlayOneShot(hitSound);
 
         GameObject GO = Instantiate(hitEffect, pos, Quaternion.identity);
+        //usuwanie efektu trafienia po 20 sekundach
         Destroy(GO, 20);
+        
+
+        //OBS£UGA ROZBRYZGU KRWI
+        // var randRotation = new Vector3(0, Random.value * 360f, 0);
+        // var dir = CalculateAngle(Vector3.forward, hit.normal);
+        float angle = Mathf.Atan2(pos.normalized.x, pos.normalized.z) * Mathf.Rad2Deg + 180;
+
+        //var effectIdx = Random.Range(0, BloodFX.Length);
+        if (effectIdx == BloodFX.Length) effectIdx = 0;
+
+        var instance = Instantiate(BloodFX[effectIdx], pos, Quaternion.Euler(0, angle + 90, 0));
+        effectIdx++;
+
+        var settings = instance.GetComponent<BFX_BloodSettings>();
+        //settings.FreezeDecalDisappearance = InfiniteDecal;
+
+        var attachBloodInstance = Instantiate(BloodAttach);
+        var bloodT = attachBloodInstance.transform;
+        bloodT.position = pos;
+        bloodT.localRotation = Quaternion.identity;
+        bloodT.localScale = Vector3.one * Random.Range(0.75f, 1.2f);
+        bloodT.LookAt(pos + pos.normalized, direction);
+        bloodT.Rotate(90, 0, 0);
+        //Destroy(attachBloodInstance, 20);
+
+        // if (!InfiniteDecal) Destroy(instance, 20);
     }
 
     // ---------- //
@@ -132,4 +159,49 @@ public class PlayerAttack : MonoBehaviour
     {
         input.Attack.started += ctx => Attack();
     }
+
+
+
+
+
+
+    public bool InfiniteDecal;
+    public bool isVR = true;
+    public GameObject BloodAttach;
+    public GameObject[] BloodFX;
+
+    public Vector3 direction;
+    int effectIdx;
+
+
+    Transform GetNearestObject(Transform hit, Vector3 hitPos)
+    {
+        var closestPos = 100f;
+        Transform closestBone = null;
+        var childs = hit.GetComponentsInChildren<Transform>();
+
+        foreach (var child in childs)
+        {
+            var dist = Vector3.Distance(child.position, hitPos);
+            if (dist < closestPos)
+            {
+                closestPos = dist;
+                closestBone = child;
+            }
+        }
+
+        var distRoot = Vector3.Distance(hit.position, hitPos);
+        if (distRoot < closestPos)
+        {
+            closestPos = distRoot;
+            closestBone = hit;
+        }
+        return closestBone;
+    }
+
+    public float CalculateAngle(Vector3 from, Vector3 to)
+    {
+        return Quaternion.FromToRotation(Vector3.up, to - from).eulerAngles.z;
+    }
+
 }
